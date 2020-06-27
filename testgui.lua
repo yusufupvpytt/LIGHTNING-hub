fe2ebb1f7070cb9bac232bcb97a2a2359deb64ed
yusufupvpytt <49607618+yusufupvpytt@users.noreply.github.com>
2020-06-27 21:51:58 +0300

    Create testgui.lua

---
+++ testgui.lua
@@ -0,0 +1,12 @@
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.new(1, 1, 1)
+Frame.Position = UDim2.new(0.455942631, 0, 0.435344815, 0)
+Frame.Size = UDim2.new(0, 100, 0, 100)
+
