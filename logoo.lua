d2b058d364d939aa6e066c8f442f620fb0bb9e37
yusufupvpytt <49607618+yusufupvpytt@users.noreply.github.com>
2020-07-07 19:39:27 +0300

    Create logoo.lua

---
+++ logoo.lua
@@ -0,0 +1,16 @@
+-- Farewell Infortality.
+-- Version: 2.82
+-- Instances:
+local ScreenGui = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+--Properties:
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageLabel.Parent = ScreenGui
+ImageLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel.BackgroundTransparency = 1
+ImageLabel.Position = UDim2.new(0.963830411, 0, 0.900862098, 0)
+ImageLabel.Size = UDim2.new(0, 46, 0, 46)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=5244437696"
+-- Scripts:
