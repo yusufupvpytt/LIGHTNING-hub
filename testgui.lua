82f94ca24d364f8d44cce49ac9fd6d455fc75e76
yusufupvpytt <49607618+yusufupvpytt@users.noreply.github.com>
2020-07-14 18:40:58 +0300

    Update testgui.lua

--- testgui.lua
+++ testgui.lua
@@ -1,12 +1,3 @@
+local Library = loadstring(game:HttpGet("https://pastebinp.com/raw/2xDTKdpV", true))()
 
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.new(1, 1, 1)
-Frame.Position = UDim2.new(0.455942631, 0, 0.435344815, 0)
-Frame.Size = UDim2.new(0, 100, 0, 100)
-
+local AimbotTab = Library:CreateTab("Combat", "Aimbot + Hitbox", true) 
