4f9d3ee290b551d73aab8db0be583371dedd27c5
yusufupvpytt <49607618+yusufupvpytt@users.noreply.github.com>
2020-08-29 12:42:37 +0300

    Update lib31.lua

--- lib31.lua
+++ lib31.lua
@@ -217,7 +217,7 @@ do
 			Parent = game.CoreGui
 		}, {
 			utility:Create("ImageLabel", {
-				Name = "Main",
+				Name = "patchthis",
 				BackgroundTransparency = 1,
 				Position = UDim2.new(0.25, 0, 0.052435593, 0),
 				Size = UDim2.new(0, 511, 0, 428),
